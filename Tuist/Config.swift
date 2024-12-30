64ac7065897cf03dd7021e7b0118a788ff0a5a00
circle0802 <hawon9781@gmail.com>
2024-12-30 17:04:03 +0900

    setting :: Plugins 생성

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -3,5 +3,8 @@ import ProjectDescription
 let config = Config(
     plugins: [
         .local(path: .relativeToManifest("../../Plugins/PmanchuIOSV2")),
+        .local(path: .relativeToManifest("../../Plugins/ConfigurationPlugin")),
+        .local(path: .relativeToManifest("../../Plugins/DependencyPlugin")),
+        .local(path: .relativeToManifest("../../Plugins/EnvironmentPlugin"))
     ]
 )
