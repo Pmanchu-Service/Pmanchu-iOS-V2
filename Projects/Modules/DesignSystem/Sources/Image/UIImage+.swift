7d5518ba646c236919e721ba1c77efb79cde23bb
circle0802 <hawon9781@gmail.com>
2024-12-31 23:24:10 +0900

    setting :: Images(Assets) 생성 및 적용

---
+++ Projects/Modules/DesignSystem/Sources/Image/UIImage+.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public extension UIImage {
+    static let logo = DesignSystemAsset.Images.logo.image
+    static let mandoo = DesignSystemAsset.Images.mandoo.image
+    static let github = DesignSystemAsset.Images.github.image
+    static let userSearch = DesignSystemAsset.Images.userSearch.image
+    static let home = DesignSystemAsset.Images.home.image
+    static let heartOutline = DesignSystemAsset.Images.heartOutline.image
+    static let heartFilled = DesignSystemAsset.Images.heartFilled.image
+    static let user = DesignSystemAsset.Images.user.image
+    static let bell = DesignSystemAsset.Images.bell.image
+    static let left = DesignSystemAsset.Images.left.image
+    static let bottom = DesignSystemAsset.Images.bottom.image
+    static let setting = DesignSystemAsset.Images.setting.image
+    static let cancel = DesignSystemAsset.Images.cancel.image
+    static let plus = DesignSystemAsset.Images.plus.image
+    static let link = DesignSystemAsset.Images.link.image
+    static let edit = DesignSystemAsset.Images.edit.image
+    static let filter = DesignSystemAsset.Images.filter.image
+    static let asterisk = DesignSystemAsset.Images.asterisk.image
+    static let check = DesignSystemAsset.Images.check.image
+    static let camera = DesignSystemAsset.Images.camera.image
+}
