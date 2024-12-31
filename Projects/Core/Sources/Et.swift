af023a600bfc46412c60680a8f58f3e413a8d043
jihoon <leejh080628@naver.com>
2025-01-01 01:58:03 +0900

    feat :: baseLabel 설정

--- Projects/Core/Sources/Et.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Et.swift
-//  Core
-//
-//  Created by 원이 on 12/30/24.
-//  Copyright © 2024 com.team.mandoo. All rights reserved.
-//
-
--- Projects/Modules/DesignSystem/Sources/Component/asdf.swift
+++
@@ -1,8 +0,0 @@
-//
-//  asdf.swift
-//  DesignSystem
-//
-//  Created by 이지훈 on 1/1/25.
-//  Copyright © 2025 com.team.mandoo. All rights reserved.
-//
-
