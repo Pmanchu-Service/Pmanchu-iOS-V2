d0fec572b5518610f563df919c049de7ae58e5e2
jihoon <leejh080628@naver.com>
2025-01-04 02:17:51 +0900

    feat :: nameVC 추가

--- Projects/Presentation/Sources/Scene/SignUp/Name/NameViewController.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Name/NameViewController.swift
@@ -1,1 +1,38 @@
+import UIKit
 
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class NameViewController: BaseViewController<NameViewModel> {
+    private let idTextField = PMTextField()
+
+    public override func attribute() {
+        super.attribute()
+        view.backgroundColor = .systemBackground
+    }
+    public override func addView() {
+        view.addSubview(idTextField)
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        attribute()
+        addView()
+        setLayout()
+    }
+
+    public override func setLayout() {
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(373)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.width.equalTo(345)
+            $0.height.equalTo(40)
+        }
+    }
+}
