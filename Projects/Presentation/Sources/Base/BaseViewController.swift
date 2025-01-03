7b08728758bec15a3193519b86fea901368745a7
circle0802 <hawon9781@gmail.com>
2025-01-03 23:21:20 +0900

    fix :: baseVC 오류 해결

--- Projects/Presentation/Sources/Base/BaseViewController.swift
+++ Projects/Presentation/Sources/Base/BaseViewController.swift
@@ -8,6 +8,16 @@ import DesignSystem
 
 open class BaseViewController<ViewModel: BaseViewModel>: UIViewController {
     public let disposeBag = DisposeBag()
+    public var viewModel: ViewModel
+    
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     open override func viewDidLoad() {
         super.viewDidLoad()
--- Projects/Presentation/Sources/Scene/SignUp/Major/MajorViewModel.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Major/MajorViewModel.swift
@@ -5,4 +5,3 @@
 //  Created by 이지훈 on 1/1/25.
 //  Copyright © 2025 com.team.mandoo. All rights reserved.
 //
-
