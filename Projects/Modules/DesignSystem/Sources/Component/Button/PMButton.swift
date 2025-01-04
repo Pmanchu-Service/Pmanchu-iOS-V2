fb11db74b979626917c35acc11300ec80d668c23
jihoon <leejh080628@naver.com>
2025-01-05 00:06:07 +0900

    feat :: PMButton 추가

---
+++ Projects/Modules/DesignSystem/Sources/Component/Button/PMButton.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+
+import Core
+
+public class PMButton: BaseButton {
+    public var buttonTap: ControlEvent<Void> {
+        return self.rx.tap
+    }
+    public override var isEnabled: Bool {
+        didSet {
+            self.attribute()
+        }
+    }
+    private var bgColor: UIColor {
+        isEnabled ? .main4 : .gray3
+    }
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    convenience public init(
+        type: UIButton.ButtonType? = .system,
+        buttonText: String? = String(),
+        isEnabled: Bool? = true,
+        isHidden: Bool? = false,
+        height: CGFloat? = 60
+    ) {
+        self.init(type: .system)
+        self.setTitle(buttonText, for: .normal)
+        self.isEnabled = isEnabled ?? true
+        self.isHidden = isHidden ?? false
+        attribute()
+
+        self.snp.remakeConstraints {
+            $0.height.equalTo(height ?? 0)
+        }
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        attribute()
+    }
+
+    public override func attribute() {
+        self.backgroundColor = bgColor
+        self.setTitleColor(.system2, for: .normal)
+        self.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        self.layer.cornerRadius = 8
+    }
+}
