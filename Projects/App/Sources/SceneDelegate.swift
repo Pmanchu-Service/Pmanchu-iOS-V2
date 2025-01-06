4cbf220c7b87b3c2d27377b9147a6af9fb3c3667
circle0802 <hawon9781@gmail.com>
2025-01-06 10:43:13 +0900

    feat :: AppFlow 설정

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -7,19 +7,22 @@ import Flow
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    private var coordinator: FlowCoordinator?
+    private var coordinator = FlowCoordinator()
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let container = Container()
-        let presentationAssembly = PresentationAssembly()
-        presentationAssembly.assemble(container: container)
-        let flow = SignUpFlow(container: container)
-        coordinator = FlowCoordinator()
-        coordinator?.coordinate(flow: flow, with: OneStepper(withSingleStep: PMStep.signUpIsRequired))
-        window?.rootViewController = flow.root as? UIViewController
-        window?.makeKeyAndVisible()
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            let window = UIWindow(windowScene: windowScene)
+        let appFlow = AppFlow(window: window, container: AppDelegate.container)
+        coordinator.coordinate(
+            flow: appFlow,
+            with: AppStepper(),
+            allowStepWhenDismissed: false
+        )
+        window.makeKeyAndVisible()
     }
 }
 func sceneDidDisconnect(_ scene: UIScene) {}
@@ -31,4 +34,3 @@ func sceneDidDisconnect(_ scene: UIScene) {}
     func sceneWillEnterForeground(_ scene: UIScene) {}
 
     func sceneDidEnterBackground(_ scene: UIScene) {}
-
