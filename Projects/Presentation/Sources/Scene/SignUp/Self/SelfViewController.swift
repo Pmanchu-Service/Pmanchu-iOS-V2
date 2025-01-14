3f7886625c3a49c0dd9c15404dcbb37354cd081a
jihoon <leejh080628@naver.com>
2025-01-14 17:46:03 +0900

    feat :: [#31] self 퍼블리싱

--- Projects/Presentation/Sources/Scene/SignUp/Self/SelfViewController.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Self/SelfViewController.swift
@@ -1,18 +1,69 @@
-//import Foundation
-//import RxSwift
-//import RxCocoa
-//import RxFlow
-//import Core
-//import Domain
-//
-//public class SignupViewModel: BaseViewModel {
-//    public struct Input {}
-//
-//    public struct Output {}
-//
-//    public func transform(input: Input) -> Output {
-//        return Output()
-//    }
-//
-//    public init() {}
-//}
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import Core
+import DesignSystem
+
+public class SelfViewController: BaseViewController<SelfViewModel> {
+    private let label = PMAuthLabelView(
+        explainText: "자기소개를 입력하세요"
+    )
+    private let selfTextField = PMTextField()
+    private let nextButton = PMButton(
+        buttonText: "다음",
+        isHidden: false
+    )
+
+    public override func attribute() {
+        super.attribute()
+        view.backgroundColor = .systemBackground
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+
+    public override func bind() {
+        let input = SelfViewModel.Input(
+            selfText: selfTextField.rx.text.orEmpty.asObservable(),
+            clickNextButton: nextButton.buttonTap.asObservable(),
+            nextButton: nextButton
+        )
+        
+        let output = viewModel.transform(input: input)
+
+        output.isNextButtonEnabled
+            .drive(nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        output.nextStep
+            .drive(onNext: { [weak self] step in
+                self?.viewModel.steps.accept(step)
+            })
+            .disposed(by: disposeBag)
+    }
+    public override func addView() {
+        [
+            label,
+            selfTextField,
+            nextButton
+        ].forEach { view.addSubview($0) }
+    }
+    
+    public override func setLayout() {
+        label.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(60)
+            $0.leading.equalToSuperview().inset(32)
+            $0.width.equalTo(224)
+            $0.height.equalTo(73)
+        }
+        selfTextField.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(40)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        nextButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(25)
+        }
+    }
+}
--- Projects/Presentation/Sources/Scene/SignUp/Self/SelfViewModel.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Self/SelfViewModel.swift
@@ -1,8 +1,40 @@
-//
-//  RankViewModel.swift
-//  Presentation
-//
-//  Created by 이지훈 on 1/1/25.
-//  Copyright © 2025 com.team.mandoo. All rights reserved.
-//
+import Foundation
+import RxSwift
+import RxCocoa
+import RxFlow
+import Core
+import Domain
+import DesignSystem
 
+public class SelfViewModel: BaseViewModel, Stepper {
+    private let disposeBag = DisposeBag()
+    public var steps = PublishRelay<Step>()
+    public struct Input {
+        let selfText: Observable<String>
+        let clickNextButton: Observable<Void>
+        let nextButton: PMButton
+    }
+    public struct Output {
+        let isNextButtonEnabled: Driver<Bool>
+        let nextStep: Driver<Step>
+    }
+
+    public func transform(input: Input) -> Output {
+        let selfText = input.selfText.share()
+        
+        let isNextButtonEnabled = selfText
+            .map { !$0.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty }
+            .asDriver(onErrorJustReturn: false)
+        
+        let nextStep = input.clickNextButton
+            .withLatestFrom(selfText)
+            .map { _ in PMStep.selfIsRequired as Step }
+            .asDriver(onErrorJustReturn: PMStep.selfIsRequired as Step)
+
+        
+        return Output(
+            isNextButtonEnabled: isNextButtonEnabled,
+            nextStep: nextStep
+        )
+    }
+}
