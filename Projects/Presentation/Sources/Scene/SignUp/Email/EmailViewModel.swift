42ea675877f62340375233794526dba5127b761f
jihoon <leejh080628@naver.com>
2025-01-14 15:39:31 +0900

    chore :: [#31] error 제거

--- Projects/Presentation/Sources/Scene/SignUp/Email/EmailViewModel.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Email/EmailViewModel.swift
@@ -9,18 +9,12 @@ import DesignSystem
 public class EmailViewModel: BaseViewModel, Stepper {
     private let disposeBag = DisposeBag()
     public var steps = PublishRelay<Step>()
-    private let emailErrorDescription = PublishRelay<String?>()
-    
     public struct Input {
         let emailText: Observable<String>
         let clickNextButton: Observable<Void>
         let nextButton: PMButton
     }
-    
-    public struct Output {
-        let emailErrorDescription: Signal<String?>
-    }
-    
+    public struct Output {}
     public func transform(input: Input) -> Output {
         let emailText = input.emailText.share()
         emailText
@@ -29,33 +23,14 @@ public class EmailViewModel: BaseViewModel, Stepper {
                 input.nextButton.isEnabled = isEnabled
             })
             .disposed(by: disposeBag)
-        
         input.clickNextButton
             .withLatestFrom(emailText)
-            .filter { [weak self] email in
-                guard let self = self else { return false }
-                return self.checkEmailData(email)
-            }
             .subscribe(onNext: { [weak self] _ in
                 guard let self = self else { return }
                 self.steps.accept(PMStep.nameIsRequired)
             })
             .disposed(by: disposeBag)
-        
-        return Output(
-            emailErrorDescription: emailErrorDescription.asSignal()
-        )
-    }
-    
-    private func checkEmailData(_ email: String) -> Bool {
-        let trimmedEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        if trimmedEmail.isEmpty {
-            emailErrorDescription.accept("이메일을 입력해주세요")
-            return false
-        }
-        
-        emailErrorDescription.accept(nil)
-        return true
+
+        return Output()
     }
 }
