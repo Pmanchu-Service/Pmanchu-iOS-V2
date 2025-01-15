4d5aab010d0b1aa98c9b317d6e794feeb153293f
jihoon <leejh080628@naver.com>
2025-01-15 20:43:12 +0900

    feat :: skillVC 추가

--- Projects/Presentation/Sources/Scene/SignUp/Skill/SkillViewController.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Skill/SkillViewController.swift
@@ -10,19 +10,50 @@ public class SkillViewController: BaseViewController<SkillViewModel> {
     private let label = PMAuthLabelView(
         explainText: "기술스택을 입력하세요"
     )
-    private let selfTextField = PMTextField()
+    private let skillTextField = PMTextField()
     private let nextButton = PMButton(
         buttonText: "다음",
         isHidden: false
     )
-    
     public override func attribute() {
         super.attribute()
         view.backgroundColor = .systemBackground
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
-    
-    
-    
-    
+    public override func bind() {
+        let input = SkillViewModel.Input(
+            skillText: skillTextField.rx.text.orEmpty.asObservable(),
+            clickNextButton: nextButton.buttonTap.asObservable(),
+            nextButton: nextButton
+        )
+        let output = viewModel.transform(input: input)
+        output.isButtonEnabled
+            .drive(nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+    }
+
+    public override func addView() {
+        [
+            label,
+            skillTextField,
+            nextButton
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        label.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(60)
+            $0.leading.equalToSuperview().inset(32)
+            $0.width.equalTo(224)
+            $0.height.equalTo(73)
+        }
+        skillTextField.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(40)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        nextButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(25)
+        }
+    }
 }
--- Projects/Presentation/Sources/Scene/SignUp/Skill/SkillViewModel.swift
+++ Projects/Presentation/Sources/Scene/SignUp/Skill/SkillViewModel.swift
@@ -6,8 +6,31 @@ import Core
 import Domain
 import DesignSystem
 
-public class EmailViewModel: BaseViewModel, Stepper {
-    
-    
-    
+public class SkillViewModel: BaseViewModel, Stepper {
+    private let disposeBag = DisposeBag()
+    public var steps = PublishRelay<Step>()
+    public struct Input {
+        let skillText: Observable<String>
+        let clickNextButton: Observable<Void>
+        let nextButton: PMButton
+    }
+    public struct Output {
+        let isButtonEnabled: Driver<Bool>
+    }
+    public func transform(input: Input) -> Output {
+        let skillText = input.skillText.share()
+        let isButtonEnabled = skillText
+            .map { !$0.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty }
+            .asDriver(onErrorJustReturn: false)
+        input.clickNextButton
+            .withLatestFrom(skillText)
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.steps.accept(PMStep.selfIsRequired)
+            })
+            .disposed(by: disposeBag)
+        return Output(
+            isButtonEnabled: isButtonEnabled
+        )
+    }
 }
