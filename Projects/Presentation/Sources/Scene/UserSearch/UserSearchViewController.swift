2fee1b01212bb9b7e6a5f34e82a016e82cb3c953
jihoon <leejh080628@naver.com>
2025-01-18 00:27:40 +0900

    feat :: userSearchVC 추가

---
+++ Projects/Presentation/Sources/Scene/UserSearch/UserSearchViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import Core
+import Domain
+import DesignSystem
+
+public class UserSearchViewController: BaseViewController<UserSearchViewModel> {
+    
+}
